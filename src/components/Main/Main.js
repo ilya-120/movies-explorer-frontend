c093e494e76e649f7c5dc8a39f38358a311f85f4
ilya120 <5374728@gmail.com>
2022-08-03 17:32:05 +0300

    "fix: исправления по ревью этап 3 фронтенд"

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -9,7 +9,7 @@ import Footer from '../Footer/Footer';
 
 function Main() {
   return (
-    <>
+    <main>
       <Header
         loggedIn={false}
       />
@@ -19,7 +19,7 @@ function Main() {
       <AboutMe />
       <Portfolio />
       <Footer />
-    </>
+    </main>
   )
 }
 
