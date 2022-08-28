b9a338b26e540280ec9bdb3a84695bac7c41dc69
ilya120 <5374728@gmail.com>
2022-08-28 08:59:08 +0300

    "feat: этап 5 фронтенд"

--- src/components/Slider/Slaider.js
+++ src/components/Slider/Slaider.js
@@ -12,7 +12,8 @@ const Slaider = () => {
     slidesToShow: 1,
     slidesToScroll: 1,
     autoplay: true,
-    autoplaySpeed: 5000
+    autoplaySpeed: 6000,
+    swipe: true
   };
   return (
     <section className='slider'>
